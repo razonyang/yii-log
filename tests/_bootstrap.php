5064b17b051a6c520f59f44f95012ac47baa39d7
razonyang <razonyang@gmail.com>
2018-06-29 19:16:21 +0800

    updated test bootstrap script

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,3 +3,6 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'test');
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+$config = require_once __DIR__ . '/config.php';
+$app = new \yii\web\Application($config);
